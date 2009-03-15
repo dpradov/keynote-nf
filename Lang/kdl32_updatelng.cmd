544b880c8acd6f6718d70f7a8104be6670b69fed
dpradov <dpradov@f29627e4-bbf2-11dd-ad2b-77eeadef73a9>
2009-03-15 20:41:46 +0000

    Adaptation of language files to version 1.7.6.1. Includes a complete Dutch translation by Ennovy and Plankje (http://forum.goeiedageven.nl/)

--- Lang/kdl32_updatelng.cmd
+++ Lang/kdl32_updatelng.cmd
@@ -9,6 +9,13 @@ del Keynote.spanish.lng.bak
 copy Lang\Keynote.spanish.lng   Lang\Keynote.spanish.lng.bak
 "..\..\src\3rd_party\kdl32_Kryvich's Delphi Localizer\lngupdate.exe" Lang\Keynote.spanish.lng  Keynote.lng -! -x
 
+del Keynote.italian.lng.bak
+copy Lang\Keynote.italian.lng   Lang\Keynote.italian.lng.bak
+"..\..\src\3rd_party\kdl32_Kryvich's Delphi Localizer\lngupdate.exe" Lang\Keynote.italian.lng  Keynote.lng -! -x
+
+del Keynote.dutch.lng.bak
+copy Lang\Keynote.dutch.lng   Lang\Keynote.dutch.lng.bak
+"..\..\src\3rd_party\kdl32_Kryvich's Delphi Localizer\lngupdate.exe" Lang\Keynote.dutch.lng  Keynote.lng -! -x
 
 pause
 @echo on
