f13d0c6cc38b0b73526b8f5925849795fa59c273
dpradov <dpradov@f29627e4-bbf2-11dd-ad2b-77eeadef73a9>
2009-10-18 21:38:34 +0000

    Preparing version 1.7.7: files '.lng', version, history.txt

--- Lang/kdl32_updatelng.cmd
+++ Lang/kdl32_updatelng.cmd
@@ -9,13 +9,21 @@ del Keynote.spanish.lng.bak
 copy Lang\Keynote.spanish.lng   Lang\Keynote.spanish.lng.bak
 "..\..\src\3rd_party\kdl32_Kryvich's Delphi Localizer\lngupdate.exe" Lang\Keynote.spanish.lng  Keynote.lng -! -x
 
-del Keynote.italian.lng.bak
-copy Lang\Keynote.italian.lng   Lang\Keynote.italian.lng.bak
-"..\..\src\3rd_party\kdl32_Kryvich's Delphi Localizer\lngupdate.exe" Lang\Keynote.italian.lng  Keynote.lng -! -x
-
 del Keynote.dutch.lng.bak
 copy Lang\Keynote.dutch.lng   Lang\Keynote.dutch.lng.bak
 "..\..\src\3rd_party\kdl32_Kryvich's Delphi Localizer\lngupdate.exe" Lang\Keynote.dutch.lng  Keynote.lng -! -x
+
+del Keynote.german.lng.bak
+copy Lang\Keynote.german.lng   Lang\Keynote.german.lng.bak
+"..\..\src\3rd_party\kdl32_Kryvich's Delphi Localizer\lngupdate.exe" Lang\Keynote.german.lng  Keynote.lng -! -x
+
+del Keynote.chinese-simplified.lng.bak
+copy Lang\Keynote.chinese-simplified.lng   Lang\Keynote.chinese-simplified.lng.bak
+"..\..\src\3rd_party\kdl32_Kryvich's Delphi Localizer\lngupdate.exe" Lang\Keynote.chinese-simplified.lng  Keynote.lng -! -x
+
+del Keynote.french.lng.bak
+copy Lang\Keynote.french.lng   Lang\Keynote.french.lng.bak
+"..\..\src\3rd_party\kdl32_Kryvich's Delphi Localizer\lngupdate.exe" Lang\Keynote.french.lng  Keynote.lng -! -x
 
 pause
 @echo on
