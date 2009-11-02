2167bd6a8b0aa5b8d92357025b966547def9a8ad
dpradov <dpradov@f29627e4-bbf2-11dd-ad2b-77eeadef73a9>
2009-11-02 21:54:04 +0000

    Preparing version 1.7.8: files '.lng', version, history.txt

--- trunk/kn_Info.pas
+++ trunk/kn_Info.pas
@@ -52,8 +52,8 @@ resourcestring
 
 const
   Program_Name     = 'KeyNote NF';
-  Program_VerStr   = '1.7.7.3';
-  Program_Version  = '1.7.7';  // Used in program caption
+  Program_VerStr   = '1.7.8.1';
+  Program_Version  = '1.7.8';  // Used in program caption
   Program_License  = 'Freeware / Open Source';
   {
   // these are now stored in GF_Const.pas
