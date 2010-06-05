7f045005580b35847869064624eca7ae1a426644
dpradov <dpradov@f29627e4-bbf2-11dd-ad2b-77eeadef73a9>
2010-06-05 15:08:14 +0000

    Implements Issue #325: Relative paths in Links (relative to .knt file)

--- trunk/kn_LinksMng.pas
+++ trunk/kn_LinksMng.pas
@@ -1131,7 +1131,7 @@ begin
             end
             else
             begin
-              myURL:= GetAbsolutePath(WideExtractFilePath(Application.ExeName), myURL);
+              myURL:= GetAbsolutePath(WideExtractFilePath(NoteFileToLoad), myURL);
               ShellExecResult := ShellExecuteW( 0, 'open', PWideChar( myURL ), nil, nil, SW_NORMAL );
             end;
           end;
