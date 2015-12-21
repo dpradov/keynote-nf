2ed7bd5df3462ee8006fc251edf991e437064a74
Daniel Prado Velasco <dpradov@users.noreply.github.com>
2015-12-21 23:33:21 +0100

    Working in 1.7.9 Beta 5, still in development
    Signed-off-by: Daniel Prado Velasco <dpradov@users.noreply.github.com>

--- general/kn_Const.pas
+++ general/kn_Const.pas
@@ -89,10 +89,10 @@ procedure DefineConst;
 
 const
   Program_Name     = 'KeyNote NF';
-  Program_Version  = '1.7.9 Beta 4';
-  Program_Version_Number  = '1.7.9.4';
-  Program_Version_Date    = '07/01/2013';
-  Program_License  = 'Free software, Open Source (Mozilla Public License)';
+  Program_Version  = '1.7.9 Beta 5';
+  Program_Version_Number  = '1.7.9.5';
+  Program_Version_Date    = '- Development -';
+  Program_License  = 'Free software, Open Source (Mozilla Public License 2.0)';
 
   Program_URL     = 'https://github.com/dpradov/keynote-nf'; //'http://keynote.prv.pl';
   Program_Email1  = 'dprado.keynote@gmail.com';
