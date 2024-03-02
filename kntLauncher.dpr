f8a7e81f39377872d9b1c86aef93624ea2092e1e
Daniel Prado Velasco <dpradov@users.noreply.github.com>
2024-03-02 15:29:02 +0100

    * Created a new add-on utility, KntLauncher.exe
      Its function is to ensure that a certain .knt file is opened in an independent instance, reusing the instance
      that may already have that file open.
    
      The utility receives the same parameters as keynote.exe.
      It assumes that "keynote.exe" is located in the same folder in which it (kntLauncher.exe) is located,
      and begins by going through all open KeyNote instances (regardless of whether they could have been opened with -dpd)
      providing them with the received command line.
      All instances will deny command line execution if they do not have the .knt file indicated on the command line open.
      If any instance has that file open, it will process the request. At a minimum that instance will be activated, restored
      and become the foreground window, but the command line will normally also include a -jmp parameter, to jump to a certain location.
      It may also involve additional execution of a macro and/or plugin.
    
      If it does not find any instance that already has the file open, it will launch the opening of a new instance, passing
      the received command line, to which it adds add the "-ignSI" and "-dnd" options.
    
      Usage examples:
         kntLauncher myHelp.knt myMacro.knm myPlugin.knl
         kntLauncher myHelp.knt -jmp"file:///*3|2"
         kntLauncher myHelp.knt -jmp"file:///*3|16|5|0|1"
         kntLauncher -jmp"file:///..\Doc\myFile.knt*3|2"

---
+++ kntLauncher.dpr
@@ -0,0 +1,110 @@
+program kntLauncher;
+
+//{.$APPTYPE CONSOLE}
+{$APPTYPE GUI}
+
+{$R *.res}
+
+uses
+  System.SysUtils,
+  Winapi.Windows,
+  Winapi.Messages,
+  kn_Msgs;
+
+var
+  _OTHER_INSTANCE_HANDLE : hwnd = 0;
+
+type
+  PFindWindowInfo = ^TFindWindowInfo;
+  TFindWindowInfo = record
+    WindowHandle: HWND;
+  end;
+
+ function CallInstance: integer; forward;
+
+
+   function EnumWindowsProc(hWnd: HWND; Info: PFindWindowInfo): BOOL; stdcall;
+   var
+     ClassName: array[0..255] of Char;
+     ResultInstance: integer;
+   begin
+     Result := True;
+
+     if GetClassName(hWnd, ClassName, SizeOf(ClassName)) > 0 then begin
+       if (StrComp(ClassName, UniqueAppName_KEYNOTE10) = 0) or (StrComp(ClassName, UniqueAppName_KEYNOTE10_dnd) = 0) then begin
+          _OTHER_INSTANCE_HANDLE:= hWnd;
+          if CallInstance = 0 then begin
+             Info^.WindowHandle := hWnd;
+             Result:= False;
+             ShowWindow(hWnd, SW_RESTORE);
+             SetForegroundWindow(hWnd);
+          end;
+       end;
+     end;
+   end;
+
+
+   function ActivateAValidKNTInstance: boolean;
+   var
+     Info: TFindWindowInfo;
+   begin
+      Info.WindowHandle := 0;
+      EnumWindows(@EnumWindowsProc, LPARAM(@Info));
+      Result:= (Info.WindowHandle <> 0);
+   end;
+
+   function CallInstance: integer;
+   var
+     CopyData : TCopyDataStruct;
+     Args: string;
+   begin
+     Args:= GetCommandLine;
+     copydata.dwData := KNT_MSG_LAUNCHER_CALL;
+     copydata.cbData:= ByteLength(Args)+1;
+     copydata.lpData := PChar(Args);
+
+     Result:= SendMessage( _OTHER_INSTANCE_HANDLE, WM_COPYDATA, 0, integer( @copydata ));
+   end;
+
+   procedure LaunchNewKNTInstance();
+   var
+     StartupInfo: TStartupInfo;
+     ProcessInfo: TProcessInformation;
+     kntPath: string;
+     commandLine: string;
+   begin
+     FillChar(StartupInfo, SizeOf(TStartupInfo), 0);
+     StartupInfo.cb := SizeOf(TStartupInfo);
+     StartupInfo.dwFlags := STARTF_USESHOWWINDOW;
+     StartupInfo.wShowWindow := SW_HIDE;
+
+     commandLine:= StringReplace(GetCommandLine, 'kntLauncher', 'keynote', []) + ' -ignSI -dnd';
+     if CreateProcess(nil, PChar(CommandLine), nil, nil, False, 0, nil, nil, StartupInfo, ProcessInfo) then begin
+        CloseHandle(ProcessInfo.hProcess);
+        CloseHandle(ProcessInfo.hThread);
+     end
+     else
+        MessageBox(0, PChar('Error launching: ' + #13#13 + commandLine), 'KntLauncher', MB_ICONWARNING or MB_OK);
+   end;
+
+
+
+begin
+  try
+    if ParamCount >= 1 then begin
+      if not ActivateAValidKNTInstance() then
+         LaunchNewKNTInstance();
+    end
+    else
+       MessageBox(0, 'Usage:'+#13#13 +'kntLauncher <kenote args>' + #13 +
+                      '(ex: kntLauncher myHelp.knt -jmp"file:///*3|16|5|0|1")', 'KntLauncher', MB_ICONINFORMATION or MB_OK);
+
+  except
+    on E: Exception do
+       MessageBox(0, PChar('Error: ' + E.Message), 'KntLauncher', MB_ICONWARNING or MB_OK);
+
+  end;
+
+
+end.
+
