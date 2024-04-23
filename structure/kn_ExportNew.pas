dpradov/keynote-nf

commit ebb4b1e94497d37b01dc23b3cbfd771ba2a89675

New option, "Show hidden marks and IDs", when exporting to plain text
The additional "Show hidden marks and IDs" option makes the hidden marks used with internal KNT Links, bookmarks and images visible.
It also makes hidden part of hyperlinks visible and shows the internal IDs of notes and nodes.
This can be useful if you are creating a help file and you want to open the file externally showing a specific location.

The hidden marks are shown in a humanized way. Example:

$11B7$12  	-> [BMK:7]
$11b7$12  	-> [TmpBMK:7]         (Bookmarks 0 - 9)
$11I7$12	-> [IMG:7]

The IDs of notes and nodes is shown enclosed in "[" and "]". Example:
My NOTE [1]
  My node B [123]

The hyperlinks are visible in the following way (eg. KNT link):

HYPERLINK "file:///*8|282|806|0|11"Text of hyperlink

* Also, %I token (Node index) now begins with 1

structure/kn_ExportNew.pas
@@ -93,6 +93,8 @@ type
     Spin_Indent: TSpinEdit;
     lblIndent: TLabel;
     CB_UseTab: TCheckBox;
+    GB_Additional: TGroupBox;
+    CB_ShowHiddenMarkers: TCheckBox;
     procedure RG_HTMLClick(Sender: TObject);
     procedure TB_OpenDlgDirClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
@@ -511,6 +513,7 @@ begin
      Tab_Options.TabVisible := (format <> xfTreePad);
      RG_NodeMode.Enabled :=    (format <> xfTreePad);
      RG_HTML.Visible := (format = xfHTML);
+     GB_Additional.Visible := (format = xfPlainText);
      CB_UseTab.Enabled:= (format = xfPlainText) and (CB_IndentNodes.Checked);
   end;
 end;
@@ -641,6 +644,7 @@ begin
       ExportSource := expSelectedNotes;
 
     ExcludeHiddenNodes:= CheckBox_ExcludeHiddenNodes.Checked;
+    ShowHiddenMarkers:= CB_ShowHiddenMarkers.Checked;
 
     TreeSelection := TTreeSelection( Combo_TreeSelection.ItemIndex );
     TargetFormat := TExportFmt( Combo_Format.ItemIndex );
@@ -688,6 +692,7 @@ begin
     end;
 
     CheckBox_ExcludeHiddenNodes.Checked:= ExcludeHiddenNodes;
+    CB_ShowHiddenMarkers.Checked:= ShowHiddenMarkers;
 
     Combo_TreeSelection.ItemIndex := ord( TreeSelection );
     Combo_Format.ItemIndex := ord( TargetFormat );
@@ -944,6 +949,8 @@ begin
 
           if ExportOptions.IncludeNoteHeadings then begin
              NoteHeading := ExpandExpTokenString( ExportOptions.NoteHeading, myNotes.Filename, RemoveAccelChar( myNote.Name ), '', 0, 0, myNote.TabSize );
+             if ExportOptions.ShowHiddenMarkers then
+                NoteHeading:= Format('%s [%d]', [NoteHeading, myNote.ID]);
              NoteHeadingRTF := MergeHeadingWithRTFTemplate( EscapeTextForRTF( NoteHeading ), NoteHeadingTpl );
           end;
 
@@ -1007,7 +1014,7 @@ begin
                       myTreeNode := TTreeNote( myNote ).TV.Items.GetFirstNode;
 
                     StartTreeNode := myTreeNode;
-                    ThisNodeIndex := 1;
+                    ThisNodeIndex := 0;
 
                     if assigned( myTreeNode ) then begin
                       StartLevel := myTreeNode.Level;
@@ -1033,6 +1040,8 @@ begin
 
                         if ExportOptions.IncludeNodeHeadings then begin
                            NodeHeading := ExpandExpTokenString( ExportOptions.NodeHeading, myNotes.Filename, RemoveAccelChar( myNote.Name ), myNoteNode.Name, myNoteNode.Level+1, ThisNodeIndex, myNote.TabSize );
+                           if ExportOptions.ShowHiddenMarkers then
+                              NodeHeading:= Format('%s [%d]', [NodeHeading, myNoteNode.ID]);
                            NodeHeadingTpl_Aux := '';
                            if ExportOptions.NodeLevelTemplates then
                               NodeHeadingTpl_Aux:= NodeLevelHeadingTpl[myNoteNode.Level];
@@ -1451,14 +1460,14 @@ end; // GetExportFilename
 
 function TForm_ExportNew.FlushExportFile( const RTF : TTabRichEdit; myNote: TTabNote; FN : string ) : boolean;
 var
-  tmpStream : TMemoryStream;
   StreamSize : integer;
   ext: string;
+  Txt: string;
   Encoding: TEncoding;
 
 begin
   result := false;
-  Encoding:= nil;
+  Encoding:= TEncoding.Default;
   FN := GetExportFilename( FN + '.EXT' );
   if ( FN = '' ) then exit; // break condition
 
@@ -1469,12 +1478,25 @@ begin
       xfPlainText : begin
         if ( ext = '' ) then FN := FN + ext_TXT;
 
+        {if ExportOptions.ShowHiddenMarkers then
+           RTF.MakeKNTHiddenMarksVisible;           // Necessary if we want to use SaveToStream, not making the hidden part of the hyperlinks visible
+        }
+
         PrepareRTFforPlainText(RTF, myNote.TabSize, EditorOptions.IndentInc);
 
-        RTF.StreamFormat := sfPlainText;
-        if not CanSaveAsANSI(RTF.Text) then
+        Txt:= RTF.Text;
+        if not CanSaveAsANSI(Text) then
            Encoding:= TEncoding.UTF8;
-        RTF.Lines.SaveToFile( FN, Encoding );
+
+        if ExportOptions.ShowHiddenMarkers then begin
+           Txt:= GetHumanizedKNTHiddenCharacters(Txt);
+           TFile.WriteAllText(FN, Txt, Encoding);
+        end
+        else begin
+           RTF.StreamFormat := sfPlainText;
+           RTF.Lines.SaveToFile( FN, Encoding );
+        end;
+
         result := true;
       end;
 

editor/kn_EditorUtils.pas
@@ -56,6 +56,7 @@ type
     function HasNonAlphaNumericOrWordDelimiter(const s : string) : boolean;
 
     function GetEditorWithNoKNTHiddenCharacters (const Editor: TTabRichEdit; HiddenMarksToRemove: THiddenMarks; const selection: boolean= true): TTabRichEdit;
+    function GetHumanizedKNTHiddenCharacters (const s: string): string;
     function RemoveKNTHiddenCharacters(const s: string; checkIfNeeded: boolean = true): string;
     function RemoveKNTHiddenCharactersInRTF(const s: AnsiString; HiddenMarks: THiddenMarks): AnsiString;
     function KeepOnlyLeadingKNTHiddenCharacters(const txt: string): string;
@@ -438,6 +439,13 @@ begin
     end
 end;
 
+function GetHumanizedKNTHiddenCharacters (const s: string): string;
+begin
+  Result:= StringReplace(s,      KNT_RTF_HIDDEN_MARK_L_CHAR + KNT_RTF_HIDDEN_BOOKMARK,   '[BMK:', [rfReplaceAll]);
+  Result:= StringReplace(Result, KNT_RTF_HIDDEN_MARK_L_CHAR + KNT_RTF_HIDDEN_Bookmark09, '[TmpBMK:', [rfReplaceAll]);
+  Result:= StringReplace(Result, KNT_RTF_HIDDEN_MARK_L_CHAR + KNT_RTF_HIDDEN_IMAGE,      '[IMG:', [rfReplaceAll]);
+  Result:= StringReplace(Result, KNT_RTF_HIDDEN_MARK_R_CHAR,                             ']', [rfReplaceAll]);
+end;
 
 function RemoveKNTHiddenCharacters(const s: string; checkIfNeeded: boolean= true): string;
 var

general/kn_INI.pas
@@ -2223,8 +2223,8 @@ begin
     TreePadRTF := false;
     TreePadSingleFile := true;
     TreeSelection := low( TTreeSelection );
-
     ExcludeHiddenNodes:= false;              // [dpv]
+    ShowHiddenMarkers:= false;
   end;
 end; // InitializeExportOptions
 

general/kn_Info.pas
@@ -432,6 +432,7 @@ type
     TreePadSingleFile : boolean;
     TreeSelection : TTreeSelection;
     ExcludeHiddenNodes: boolean;    // [dpv]
+    ShowHiddenMarkers: boolean;
   end;
 
 type

structure/kn_ExportNew.dfm
@@ -55,7 +55,7 @@ object Form_ExportNew: TForm_ExportNew
     Top = 5
     Width = 311
     Height = 348
-    ActivePage = Tab_Main
+    ActivePage = Tab_Options
     HotTrack = False
     TabInactiveColor = clBtnFace
     TabInactiveFont.Charset = DEFAULT_CHARSET
@@ -166,8 +166,6 @@ object Form_ExportNew: TForm_ExportNew
           AllowAllUp = True
           GroupIndex = 3
           Flat = False
-          Glyph.Data = {00000000}
-          GlyphMask.Data = {00000000}
           ImageIndex = 1
           Images = Form_Main.IMG_Toolbar
           RepeatInterval = 101
@@ -437,6 +435,27 @@ object Form_ExportNew: TForm_ExportNew
         Images = Form_Main.IMG_Toolbar
         TabOrder = 3
         OnClick = Btn_TknHlpClick
+      end
+      object GB_Additional: TGroupBox
+        Left = 3
+        Top = 233
+        Width = 292
+        Height = 81
+        Caption = ' Other options '
+        TabOrder = 4
+        Visible = False
+        object CB_ShowHiddenMarkers: TCheckBox
+          Left = 12
+          Top = 24
+          Width = 253
+          Height = 17
+          Hint = 
+            'Makes hidden marks (KNT Links, bmk and img) visible'#13#10'Also shows ' +
+            'Note and Node IDs'
+          Caption = 'Show hidden marks and IDs'
+          TabOrder = 0
+          OnClick = CB_IndentNodesClick
+        end
       end
     end
     object Tab_TreePad: TTab95Sheet

structure/kn_NoteObj.pas
@@ -265,7 +265,9 @@ type
                                  ) : string;
     procedure GetLinkAtCursor(var URL: string; var TextURL: string; var LeftE: integer; var RightE: integer; SelectURL: boolean= true);
     function ParagraphsSelected: Boolean;
+    function ChangeKNTHiddenMarksVisibility(Hide: boolean; Selection: boolean = true): boolean;
     procedure HideKNTHiddenMarks(Selection: boolean = true);
+    function MakeKNTHiddenMarksVisible: boolean;
     procedure RemoveKNTHiddenCharacters (selection: boolean= true);
 
   end; // TTabRichEdit
@@ -2121,13 +2123,15 @@ begin
 end;   // GetWordAtCursor
 
 
-procedure TTabRichEdit.HideKNTHiddenMarks(Selection: boolean = true);
+function TTabRichEdit.ChangeKNTHiddenMarksVisibility(Hide: boolean; Selection: boolean = true): boolean;
 var
    p, pF: integer;
-   SS, SL: integer;
+   SS, SL, Offset: integer;
    Str: String;
 begin
    // {\rtf1\ansi {\v\'11B5\'12} XXX };   {\rtf1\ansi \v\'11B5\'12\v0 XXX};  {\rtf1\ansi \v\'11T999999\'12\v0 XXX};   '#$11'B5'#$12'
+
+  Result:= false;
 
   Str:= TextPlain(Selection);
   p:= Pos(KNT_RTF_HIDDEN_MARK_L_CHAR, Str, 1);
@@ -2135,6 +2139,10 @@ begin
 
   SS:= SelStart;
   SL:= SelLength;
+  if Selection then
+     Offset:= SS
+  else
+     Offset:= 0;
 
   BeginUpdate;
   SuspendUndo;
@@ -2143,9 +2151,10 @@ begin
        if p > 0 then begin
           pF:= Pos(KNT_RTF_HIDDEN_MARK_R_CHAR, Str, p+3);
           if (pF > 0) and (pF-p <= KNT_RTF_HIDDEN_MAX_LENGHT_CHAR) then begin
-             SetSelection(p+SS-1, pF+SS, true);
-             SelAttributes.Hidden:= True;
+             SetSelection(p+Offset-1, pF+Offset, true);
+             SelAttributes.Hidden:= Hide;
              p:= pF + 1;
+             Result:= true;
           end;
           p:= Pos(KNT_RTF_HIDDEN_MARK_L_CHAR, Str, p);
        end;
@@ -2158,6 +2167,17 @@ begin
   end;
 
 end;   // HideKNTHiddenMarks
+
+
+procedure TTabRichEdit.HideKNTHiddenMarks(Selection: boolean = true);
+begin
+   ChangeKNTHiddenMarksVisibility(True, Selection);
+end;
+
+function TTabRichEdit.MakeKNTHiddenMarksVisible: boolean;
+begin
+    Result:= ChangeKNTHiddenMarksVisibility(False, False);
+end;
 
 
 procedure TTabRichEdit.RemoveKNTHiddenCharacters (selection: boolean= true);

commit e030576124a3a61dc4f0f029180f98a5bf95853f

Fixed: Regression in exporting nodes with non ANSI characters to plain text
Commit ebb4b1e94497d3 ( New option, "Show hidden marks and IDs", when exporting to plain text )
introduced a bug where exporting to plain text to not work with Unicode:

        if not CanSaveAsANSI(RTF.Text) then
           Encoding:= TEncoding.UTF8;

        ->
        Txt:= RTF.Text;
        if not CanSaveAsANSI(Text) then   // It should be  ... CanSaveAsANSI(Txt) ...
           Encoding:= TEncoding.UTF8;

structure/kn_ExportNew.pas
@@ -1459,7 +1459,7 @@ begin
         PrepareRTFforPlainText(RTF, myFolder.TabSize, EditorOptions.IndentInc);
 
         Txt:= RTF.Text;
-        if not CanSaveAsANSI(Text) then
+        if not CanSaveAsANSI(Txt) then
            Encoding:= TEncoding.UTF8;
 
         if ShowHiddenMarkers then begin