dpradov/keynote-nf

commit 175fe359d157a009b9d68f42b1d4ec235ed61a1f

New: Unregistered images can also be displayed in internal Image Viewer
Images without an associated ID, in Scratchpad or in a note that doesn't support registered images,
like virtual RTF nodes or notes in a EmbeddedRTF image storage format file, can now be displayed using
the Image Viewer (double clicking the image)

editor/kn_ImagesMng.pas
@@ -82,6 +82,7 @@ resourcestring
            '  Content lost' + #13 +
            '  Will be removed from Images';
 
+  STR_22 = '< Unregistered image >';
 
 const
   SEP = '|';
@@ -420,6 +421,7 @@ type
 
     procedure ReloadImageStream (Img: TKntImage);
     procedure CheckFreeImageStreamsNotRecentlyUsed;
+    procedure FreeImage (Img: TKntImage);
     function RecalcNextID: boolean;
 
     procedure InsertImage (FileName: String; Editor: TKntRichEdit; Owned: boolean; const NameProposed: string = '');
@@ -444,6 +446,7 @@ type
                                  ): AnsiString; overload;
 
     function ProcessImagesInClipboard(Editor: TRxRichEdit; const FolderName: string; SelStartBeforePaste: integer; FirstImageID: integer= 0): boolean;
+    function TryRTFPictToImage (Buffer: Pointer; BufSize: integer; var Img: TKntImage): boolean;
 
     procedure ResetAllImagesCountReferences;
     procedure RemoveImagesReferences (const IDs: TImageIDs);
@@ -474,7 +477,7 @@ type
     property ImgViewerIsOpen: boolean read GetImgViewerIsOpen;
     property ImgIDinViewer: integer read GetImgIDinViewer;
     procedure OpenImageFile(FilePath: string);
-    procedure OpenImageViewer (ImgID: integer; ShowExternalViewer: boolean; SetLastFormImageOpened: boolean);
+    procedure OpenImageViewer (ImgID: integer; ShowExternalViewer: boolean; SetLastFormImageOpened: boolean; Img: TKntImage = nil);
     procedure CheckBringToFrontLastImageViewer;
   end;
 
@@ -2312,6 +2315,13 @@ begin
 
 end;
 
+procedure TImageMng.FreeImage (Img: TKntImage);
+begin
+   if assigned(Img) and (Img.ID = 0) then
+      Img.FreeImageStream;
+end;
+
+
 function TImageMng.RecalcNextID: boolean;
 var
    NewID: integer;
@@ -3301,6 +3311,41 @@ begin
 end;
 
 
+function TImageMng.TryRTFPictToImage (Buffer: Pointer; BufSize: integer; var Img: TKntImage): boolean;
+var
+  pPict, pRTFImageEnd: integer;
+  Stream: TMemoryStream;
+  Width, Height, WidthGoal, HeightGoal: integer;
+  ImgFormat: TImageFormat;
+
+  ImgID: integer;
+  RTFText: PAnsiChar;
+
+begin
+   Img:= nil;
+   RTFText:= PAnsiChar(Buffer);
+
+   if (Length(RTFText) > BufSize) then begin
+      assert(Length(RTFText) <= BufSize );
+      exit;
+   end;
+
+   pPict:= PosPAnsiChar('{\pict{', RTFText, 1) -1;
+   if pPict = -1 then exit;
+
+   Stream:= TMemoryStream.Create;
+   try
+      if RTFPictToImage (RTFText, pPict, Stream, ImgFormat, Width, Height, WidthGoal, HeightGoal, pRTFImageEnd, true) then begin
+         Img:= TKntImage.Create(0, '', true, ImgFormat, Width, Height, 0,0, Stream);
+         Img.Caption:= STR_22;
+      end;
+
+   finally
+      if Img = nil then
+         Stream.Free;
+   end;
+
+end;
 
 
 //-----------------------------------------
@@ -3774,18 +3819,18 @@ begin
 
 end;
 
-procedure TImageMng.OpenImageViewer (ImgID: integer; ShowExternalViewer: boolean; SetLastFormImageOpened: boolean);
+procedure TImageMng.OpenImageViewer (ImgID: integer; ShowExternalViewer: boolean; SetLastFormImageOpened: boolean; Img: TKntImage = nil);
 var
   Form_Image, OpenedViewer: TForm_Image;
-  Img: TKntImage;
   FilePath: string;
   UsingOpenViewer: boolean;
 
 begin
-   if ImgID = 0 then exit;
+   if (ImgID = 0) and not assigned(Img) then exit;
 
    try
-      Img:= GetImageFromID(ImgID);
+      if not assigned(Img) then
+         Img:= GetImageFromID(ImgID);
       if Img= nil then exit;
 
       if ShowExternalViewer then begin
@@ -3793,7 +3838,8 @@ begin
          OpenImageFile(FilePath);
       end
       else begin
-         ActiveFolder.EditorToDataStream;
+         if ActiveEditor.NoteObj <> nil then
+            ActiveFolder.EditorToDataStream;
 
          UsingOpenViewer:= false;
 

ui/knt.ui.editor.pas
@@ -258,6 +258,7 @@ uses
    kn_EditorUtils,
    kn_RTFUtils,
    kn_ClipUtils,
+   kn_ImagesMng,
    kn_ImagesUtils,
    kn_MacroMng,
    kn_NoteFileMng,
@@ -1590,9 +1591,11 @@ var
   ImgID, p, SL: integer;
   txt: AnsiString;
   ImgIDs: TImageIDs;
+  Img: TKntImage;
 
 begin
    ImgID:= 0;
+   Img:= nil;
    if FSupportsRegisteredImages then begin
       SL:= SelLength;
       if SL = 1 then
@@ -1608,8 +1611,14 @@ begin
       end;
    end;
 
-   if ImgID <> 0 then
-      ImageMng.OpenImageViewer(ImgID, CtrlDown, false);
+   if (ImgID = 0) and FSupportsImages and (not FSupportsRegisteredImages or not assigned(NoteObj) ) then begin
+      txt:= RtfSelText;
+      if length(txt) > 0 then
+         ImageMng.TryRTFPictToImage (@txt[1], Length(txt), Img);
+   end;
+
+   if (ImgID <> 0) or (Img <> nil) then
+      ImageMng.OpenImageViewer(ImgID, false, false, Img);
 
    inherited;
 end;

editor/kn_ImageForm.pas
@@ -115,6 +115,7 @@ type
     procedure SetImage(value: TKntImage);
     procedure ChangeImage;
     procedure ConfigureAndShowImage (KeepWindowSize: boolean);
+    procedure CheckClearUnregisteredImage;
     procedure ResizeImage;
     procedure DoExpand(Expand: boolean);
     procedure UpdatePositionAndZoom;
@@ -226,6 +227,7 @@ end;
 
 procedure TForm_Image.FormClose(Sender: TObject; var Action: TCloseAction);
 begin
+   CheckClearUnregisteredImage;
    Free;
 end;
 
@@ -296,15 +298,25 @@ procedure TForm_Image.SetImage(value: TKntImage);
 begin
    if value = nil then exit;
 
+   CheckClearUnregisteredImage;
+
    fImage:= value;
    fCurrentKntFile:= KntFile;
    fImageID:= fImage.ID;
+
    if Visible then begin
      ConfigureAndShowImage (true);
      WindowState:= wsNormal;
    end;
 end;
 
+procedure TForm_Image.CheckClearUnregisteredImage;
+begin
+   if assigned(fImage) and (fImage.ID = 0) then begin
+      ImageMng.FreeImage(fImage);
+      fImage.Free;
+   end;
+end;
 
 procedure TForm_Image.FormShow(Sender: TObject);
 begin
@@ -327,6 +339,7 @@ var
   W, H, MaxW, MaxH: integer;
   Ratio: Single;
   ImageSizerThanWindow: boolean;
+  RegisteredImg: boolean;
 
 begin
    if Image = nil then exit;
@@ -338,10 +351,17 @@ begin
    lblLinked.Visible:= not Image.IsOwned;
    cScrollBox.Color:= KeyOptions.ImgViewerBGColor;
 
+   RegisteredImg:= (fImageID <> 0);
+   txtCaption.Enabled:= RegisteredImg;
+
    fImagePath:= ImageMng.GetImagePath(Image);
-   if fImagePath <> '' then begin
+   if RegisteredImg and (fImagePath <> '') then begin
       btnOpenFolder.Enabled:= true;
       btnOpenFolder.Hint:= STR_02 + '   ' + fImagePath;
+   end
+   else begin
+      btnOpenFolder.Enabled:= false;
+      btnOpenFolder.Hint:= '';
    end;
 
    if Image.Caption <> '' then

commit 85aedcaebe682f365b7b5fbf7f99ff5057b9ed80

Fixed: Ctrl+DblClick on a visible image didn't open the image outside KNT, but with internal viewer
As indicated in the help file the behavior should be:
If the image is visible:
- Double click on it opens the internal KNT viewer with that image loaded, displayed at 100% (or reduced if it cannot be displayed in full on the screen)
- Ctrl + double click directly opens the image from outside KNT, with the program that is associated with its extension (This is only possible with Linked images or images saved in an external Folder storage)

Now, if it is not possible to open the image with an external viewer, the internal one is used.

Error was introduced in commit 175fe359d157a009b9 (* New: Unregistered images can also be displayed in internal Image Viewer)

editor/kn_ImagesMng.pas
@@ -3868,37 +3868,39 @@ begin
 
       if ShowExternalViewer then begin
          FilePath:= GetImagePath(Img);
-         OpenImageFile(FilePath);
+         if FileExists(FilePath) then begin
+            OpenImageFile(FilePath);
+            exit;
+         end;
+      end;
+
+      if ActiveEditor.NNodeObj <> nil then
+         ActiveFolder.SaveEditorToDataModel;
+
+      UsingOpenViewer:= false;
+
+      OpenedViewer:= ImgViewerInstance;
+      if (OpenedViewer <> nil) and (KeyOptions.ImgSingleViewerInstance) then begin
+         Form_Image:= OpenedViewer;
+         UsingOpenViewer:= true;
       end
       else begin
-         if ActiveEditor.NNodeObj <> nil then
-            ActiveFolder.SaveEditorToDataModel;
-
-         UsingOpenViewer:= false;
-
-         OpenedViewer:= ImgViewerInstance;
-         if (OpenedViewer <> nil) and (KeyOptions.ImgSingleViewerInstance) then begin
-            Form_Image:= OpenedViewer;
-            UsingOpenViewer:= true;
-         end
-         else begin
-            Form_Image := TForm_Image.Create( Form_Main );
-            NewImageViewer(Form_Image);
-         end;
-
-         { We use kn_ImageForm.LastFormImageOpened to be able to give focus to the viewer from Form_Main.RxRTFMouseUp,
-           since it is lost if we open the viewer by right-clicking on a link. But we do not want to lose focus on the
-           main screen if KeyOptions.ImgSingleViewerInstance=True and the viewer had already been created previously
-           (it was visible before clicking on the link) to, among others, favor being able to implement the
-           ImgHotTrackViewer option. For this we look at ImgViewerInstance
-         }
-         if SetLastFormImageOpened and (OpenedViewer = nil) then
-            kn_ImageForm.LastImgViewerOpen:= Form_Image;
-         Form_Image.Image:= Img;
-         Form_Image.Folder:= ActiveFolder;
-         if not UsingOpenViewer then
-            Form_Image.Show;
+         Form_Image := TForm_Image.Create( Form_Main );
+         NewImageViewer(Form_Image);
       end;
+
+      { We use kn_ImageForm.LastFormImageOpened to be able to give focus to the viewer from Form_Main.RxRTFMouseUp,
+        since it is lost if we open the viewer by right-clicking on a link. But we do not want to lose focus on the
+        main screen if KeyOptions.ImgSingleViewerInstance=True and the viewer had already been created previously
+        (it was visible before clicking on the link) to, among others, favor being able to implement the
+        ImgHotTrackViewer option. For this we look at ImgViewerInstance
+      }
+      if SetLastFormImageOpened and (OpenedViewer = nil) then
+         kn_ImageForm.LastImgViewerOpen:= Form_Image;
+      Form_Image.Image:= Img;
+      Form_Image.Folder:= ActiveFolder;
+      if not UsingOpenViewer then
+         Form_Image.Show;
 
    except
      on E: Exception do

ui/knt.ui.editor.pas
@@ -2640,7 +2640,7 @@ begin
    end;
 
    if (ImgID <> 0) or (Img <> nil) then
-      ImageMng.OpenImageViewer(ImgID, false, false, Img);
+      ImageMng.OpenImageViewer(ImgID, CtrlDown, false, Img);
 
    inherited;
 end;