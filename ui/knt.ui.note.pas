dpradov/keynote-nf

commit bcd8ed5ae443f60a151d968a075fd974369468d9

Fixed: When saving a file, either manually or automatically, any floating editors will not close.
Changes can be made in these floating editors. Those changes will be saved, but the editors will remain open, unaffected.

ui/knt.ui.note.pas
@@ -717,7 +717,7 @@ begin
 
   if assigned(NNode) then begin
      if FEditor.FloatingEditor <> nil then
-        FEditor.HideNestedFloatingEditor;
+        FEditor.DoSaveChangesInFloatingEditor;
 
      NEntry:= Note.Entries[0];
 

ui/knt.ui.editor.pas
@@ -222,7 +222,8 @@ type
     procedure Unfold;
     procedure PreviewFoldedBlock (SS: integer);
     procedure HideNestedFloatingEditor;
-    procedure HidingFloatingEditor;
+    procedure DoSaveChangesInFloatingEditor;
+    procedure SaveChangesFromFloatingEditor (HidingFloatingEditor: boolean);
     function Focused: boolean; override;
 
     function VinculatedNNode(var NNodeObj, NEntryObj, FolderObj: TObject): boolean;
@@ -2779,7 +2780,8 @@ begin
   end;
 end;
 
-procedure TKntRichEdit.HidingFloatingEditor;
+
+procedure TKntRichEdit.SaveChangesFromFloatingEditor (HidingFloatingEditor: boolean);
 var
   FE: TFloatingEditor;
   pI, pF: integer;
@@ -2788,12 +2790,14 @@ var
 
 begin
   if FloatingEditor <> nil then begin
-     FormParent:= Form_Main;
-     if ParentEditor = nil then
-        FormParent:= Form_Main
-     else
-        FormParent:= TForm(Self.Parent);
-     TagMng.CreateTagSelector(FormParent);
+     if HidingFloatingEditor then begin
+       FormParent:= Form_Main;
+       if ParentEditor = nil then
+          FormParent:= Form_Main
+       else
+          FormParent:= TForm(Self.Parent);
+       TagMng.CreateTagSelector(FormParent);
+     end;
 
      FE:= TFloatingEditor(FloatingEditor);
      if not FE.Editor.Modified then exit;
@@ -2813,6 +2817,13 @@ begin
      end;
 
   end;
+end;
+
+
+procedure TKntRichEdit.DoSaveChangesInFloatingEditor;
+begin
+  if FloatingEditor <> nil then
+     TFloatingEditor(FloatingEditor).SaveChangesToParentEditor;
 end;
 
 

ui/knt.ui.floatingEditor.pas
@@ -58,6 +58,8 @@ type
     procedure ShowEditor(X, Y: Integer; FontHeight: integer);
     procedure HideEditor;
     procedure HideNestedFloatingEditor;
+
+    procedure SaveChangesToParentEditor;
   end;
 
 
@@ -188,7 +190,7 @@ begin
   TagMng.FreeTagSelector;
   HideNestedFloatingEditor;
 
-  fParentEditor.HidingFloatingEditor;
+  fParentEditor.SaveChangesFromFloatingEditor(True);       // True -> HidingFloatingEditor: True
   Hide();
 end;
 
@@ -198,6 +200,14 @@ begin
      TFloatingEditor(Editor.FloatingEditor).HideEditor;
      FreeAndNil(Editor.FloatingEditor);
   end;
+end;
+
+procedure TFloatingEditor.SaveChangesToParentEditor;
+begin
+    if Editor.FloatingEditor <> nil then
+       TFloatingEditor(Editor.FloatingEditor).SaveChangesToParentEditor;    // Recursive
+
+    fParentEditor.SaveChangesFromFloatingEditor(False);    // HidingFloatingEditor: False
 end;
 
 

commit 81c7f1e99e299bc5a12b914399aebe08daba4595

* Fixed: Floating editor should be closed when selected another node in the same folder   (bug introduced in commit bcd8ed5ae, 18/09/25 : "Fixed: When saving a file, either manually or automatically, any floating editors will not close.")
Also: Entries: Fixed the warnings when changes in floating editor cannot be saved (because include nested cells)

ui/knt.ui.note.pas
@@ -633,10 +633,11 @@ procedure TKntNoteUI.NEntryUIEditorEnter(Sender: TObject);
 var
   p: TNEntriesPanel;
 begin
-   for p := Low(TNEntriesPanel) to High(TNEntriesPanel) do
-      if FNEntryUI[p] <> nil then
-         if not FNEntryUI[p].HideNestedFloatingEditor then
-            exit;
+   if not FloatingEditorCannotBeSaved then
+      for p := Low(TNEntriesPanel) to High(TNEntriesPanel) do
+        if FNEntryUI[p] <> nil then
+           if not FNEntryUI[p].HideNestedFloatingEditor then
+              exit;
 
   Timer.Enabled:= True;
 end;
@@ -700,6 +701,8 @@ begin
    if SavePreviousContent and (FNNode <> nil) then
       SaveToDataModel;
 
+   if FloatingEditorCannotBeSaved then exit;
+
    FNNode:= NNode;
    FNNodeUIConfig:= nil;
 
@@ -713,8 +716,11 @@ begin
      end;
    end;
 
-   for p := Low(TNEntriesPanel) to High(TNEntriesPanel) do
+   for p := Low(TNEntriesPanel) to High(TNEntriesPanel) do begin
       ShowPanel[p]:= false;
+      if (FNEntryUI[p] <> nil) and not FNEntryUI[p].HideNestedFloatingEditor then
+         exit;
+   end;
 
    for i := 0 to High(FNNodeUIConfig.PanelsConfig) do begin
        PanelConfig:= FNNodeUIConfig.PanelsConfig[i];

ui/knt.ui.noteEntries.pas
@@ -312,13 +312,13 @@ begin
   if Assigned(FOnEnterOnEditor) then
     FOnEnterOnEditor(Self);
 
+  TKntNoteUI(FNoteUI).NEntryUIEditorEnter(Self);
+
   if FloatingEditorCannotBeSaved then
      Editor.ActivateFloatingEditor;
 
   if not FInfoPanelHidden and FPanelConfig.Auxiliar then
      pnlIdentif.Visible := True;
-
-  TKntNoteUI(FNoteUI).NEntryUIEditorEnter(Self);
 end;
 
 
@@ -554,8 +554,10 @@ begin
 
    try
      try
-       if SavePreviousContent and not FNoteUI.GetNNodeDeleted then
+       if SavePreviousContent and not FNoteUI.GetNNodeDeleted then begin
           SaveToDataModel();
+          Editor.HideNestedFloatingEditor;
+       end;
 
        FNNode:= NNode;
        //FNNodeDeleted:= false;    //##

ui/knt.ui.tree.pas
@@ -1637,7 +1637,6 @@ begin
      end
      else begin
         TV.FocusedNode:= fLastNodeSelected;
-        TKntFolder(Folder).NodeSelected(fLastNodeSelected, nil);
         TV.Selected[Node]:= False;
         TV.Selected[TV.FocusedNode]:= True;
      end;