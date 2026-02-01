418c792d4427a9323e6c1a376f42f2796ede8a53
Daniel Prado Velasco <dpradov@users.noreply.github.com>
2026-02-01 19:14:44 +0100

    New: Added protection against basic/intermediate keyloggers
    After each user keystroke in a protected field (password), TSecureText, between 0 and 3
    fake keystrokes are generated, each containing real events that the keyloggers must capture.
    These fake keystrokes are ignored by the control in KeyNote NF and do not affect the actual
    password entered.
    Both the number of keystrokes and the fake keystrokes are generated randomly.

--- ui/knt.ui.secureEdit.pas
+++ ui/knt.ui.secureEdit.pas
@@ -33,24 +33,39 @@ type
   private
     FSecureMode: boolean;
     FSecureText: string;
+    FObfuscationEnabled: Boolean;
+    FIgnoringFakeKeys: Boolean;
+    //FTestKeys: string;
     procedure WMGetText(var Message: TMessage); message WM_GETTEXT;
     procedure WMGetTextLength(var Message: TMessage); message WM_GETTEXTLENGTH;
+    procedure WMKeyDown(var Message: TWMKeyDown); message WM_KEYDOWN;
     //procedure WMPaste(var Message: TMessage); message WM_PASTE;
   protected
     procedure KeyPress(var Key: Char); override;
     procedure KeyDown(var Key: Word; Shift: TShiftState); override;
     procedure SetSecureMode(Value: boolean);
+    procedure SendFakeKeystrokes;
 
   public
     property SecureMode: boolean read FSecureMode write SetSecureMode;
+    property ObfuscationEnabled: Boolean read FObfuscationEnabled write FObfuscationEnabled;
     function GetSecureText: string;
     procedure ClearSecure;
     constructor Create(AOwner: TComponent); override;
   end;
 
+{$IFDEF INCLUDE_POSTKEYEX}
+  Procedure PostKeyEx( hWindow: HWnd; key: Word; Const shift: TShiftState; specialkey: Boolean );
+{$ENDIF}
+
+
 procedure Register;
 
 implementation
+{$IFNDEF INCLUDE_POSTKEYEX}
+uses
+  gf_miscvcl;
+{$ENDIF}
 
 
 constructor TSecureEdit.Create(AOwner: TComponent);
@@ -59,6 +74,9 @@ begin
   FSecureText := '';
   FSecureMode:= True;
   PasswordChar := '*';
+  FIgnoringFakeKeys := False;
+  FObfuscationEnabled:= True;
+  Randomize;
 end;
 
 
@@ -81,6 +99,16 @@ begin
 end;
 
 
+procedure TSecureEdit.WMKeyDown(var Message: TWMKeyDown);
+begin
+  if FIgnoringFakeKeys then begin
+    Message.Result := 0;
+    Exit;
+  end;
+
+  inherited;
+end;
+
 procedure TSecureEdit.KeyPress(var Key: Char);
 var
   CursorPos: Integer;
@@ -88,7 +116,13 @@ begin
   if not FSecureMode then begin
      inherited;
      exit;
+  end
+  else
+  if FIgnoringFakeKeys then begin
+    Key := #0;
+    Exit;
   end;
+
 
   CursorPos := SelStart;
 
@@ -99,8 +133,10 @@ begin
        Delete(FSecureText, CursorPos, 1);
      Key := #0;
      inherited KeyPress(Key);
+
+     SendFakeKeystrokes;            // Send fake keys after backspace
   end
-  else if Key >= #32 then begin    // Carácter normal
+  else if Key >= #32 then begin    // Normal character
      if SelLength > 0 then begin
        Delete(FSecureText, CursorPos + 1, SelLength);
        Insert(Key, FSecureText, CursorPos + 1);
@@ -109,6 +145,9 @@ begin
        Insert(Key, FSecureText, CursorPos + 1);
      Key := '*';
      inherited KeyPress(Key);
+
+     // THEN we send fake characters
+     SendFakeKeystrokes;
   end
   else
      inherited KeyPress(Key);
@@ -122,6 +161,11 @@ begin
   if not FSecureMode then begin
      inherited;
      exit;
+  end
+  else
+  if FIgnoringFakeKeys then begin
+    Key := 0;
+    Exit;
   end;
 
   CursorPos := SelStart;
@@ -133,6 +177,8 @@ begin
        Delete(FSecureText, CursorPos + 1, 1);
 
      inherited;
+
+     SendFakeKeystrokes;
      Exit;
   end;
 
@@ -192,6 +238,140 @@ begin
 end;
 
 
+procedure TSecureEdit.SendFakeKeystrokes;
+var
+  i, NumFakes: Integer;
+  FakeKey: Word;
+begin
+  if not FObfuscationEnabled then Exit;
+
+  //FTestKeys:= FTestKeys + '|';
+
+  if Random(10) < 2 then Exit;  // 20% of the time, send nothing.
+
+  NumFakes := Random(4);        // 0-3 characters
+  FIgnoringFakeKeys := True;
+
+  try
+      for i := 1 to NumFakes do  begin
+        case Random(3) of
+          0: FakeKey := Ord('A') + Random(26);   // A-Z
+          1: FakeKey := Ord('0') + Random(10);   // 0-9
+          2: FakeKey := VK_SPACE + Random(15);   // Various symbols
+        end;
+
+        PostKeyEx(Handle, FakeKey, [], False);
+        //FTestKeys:= FTestKeys + Char(FakeKey);
+      end;
+      Application.ProcessMessages;
+
+  finally
+    FIgnoringFakeKeys := False;
+  end;
+
+end;
+
+
+{$IFDEF INCLUDE_POSTKEYEX}
+
+{************************************************************
+ * Procedure PostKeyEx
+ *
+ * Parameters:
+ *  hWindow: target window to be send the keystroke
+ *  key    : virtual keycode of the key to send. For printable
+ *           keys this is simply the ANSI code (Ord(character)).
+ *           For letters alsways use the upper-case version!
+ *  shift  : state of the modifier keys. This is a set, so you
+ *           can set several of these keys (shift, control, alt,
+ *           mouse buttons) in tandem. The TShiftState type is
+ *           declared in the Classes Unit.
+ *  specialkey: normally this should be False. Set it to True to
+ *           specify a key on the numeric keypad, for example.
+ *           If this parameter is true, bit 24 of the lparam for
+ *           the posted WM_KEY* messages will be set.
+ * Description:
+ *  This procedure sets up Windows key state array to correctly
+ *  reflect the requested pattern of modifier keys and then posts
+ *  a WM_KEYDOWN/WM_KEYUP message pair to the target window. Then
+ *  Application.ProcessMessages is called to process the messages
+ *  before the keyboard state is restored.
+ * Error Conditions:
+ *  May fail due to lack of memory for the two key state buffers.
+ *  Will raise an exception in this case.
+ * NOTE:
+ *  Setting the keyboard state will not work across applications
+ *  running in different memory spaces on Win32.
+ *Created: 02/21/96 16:39:00 by P. Below
+ ************************************************************}
+Procedure PostKeyEx( hWindow: HWnd; key: Word; Const shift: TShiftState; specialkey: Boolean );
+Type
+  TBuffers = Array [0..1] of TKeyboardState;
+Var
+  pKeyBuffers : ^TBuffers;
+  LP: LPARAM;
+Begin
+  (* check if the target window exists *)
+  If IsWindow(hWindow) Then Begin
+    (* set local variables to default values *)
+    pKeyBuffers := Nil;
+    LP := MakeLParam(0, MapVirtualKey(key, 0));
+
+    (* modify lparam if special key requested *)
+    If specialkey Then
+      LP := LP or $1000000;
+
+    (* allocate space for the key state buffers *)
+    New(pKeyBuffers);
+    try
+      (* Fill buffer 1 with current state so we can later restore it.
+         Null out buffer 0 to get a "no key pressed" state. *)
+      GetKeyboardState( pKeyBuffers^[1] );
+      FillChar(pKeyBuffers^[0], Sizeof(TKeyboardState), 0);
+
+      (* set the requested modifier keys to "down" state in the buffer *)
+      If ssShift In shift Then
+        pKeyBuffers^[0][VK_SHIFT] := $80;
+      If ssAlt In shift Then Begin
+        (* Alt needs special treatment since a bit in lparam needs also be set *)
+        pKeyBuffers^[0][VK_MENU] := $80;
+        LP := LP or $20000000;
+      End;
+      If ssCtrl In shift Then
+        pKeyBuffers^[0][VK_CONTROL] := $80;
+      If ssLeft In shift Then
+        pKeyBuffers^[0][VK_LBUTTON] := $80;
+      If ssRight In shift Then
+        pKeyBuffers^[0][VK_RBUTTON] := $80;
+      If ssMiddle In shift Then
+        pKeyBuffers^[0][VK_MBUTTON] := $80;
+
+      (* make out new key state array the active key state map *)
+      SetKeyboardState( pKeyBuffers^[0] );
+
+      (* post the key messages *)
+      If ssAlt In Shift Then Begin
+        PostMessage( hWindow, WM_SYSKEYDOWN, key, LP);
+        PostMessage( hWindow, WM_SYSKEYUP, key, LP or $C0000000);
+      End
+      Else Begin
+        PostMessage( hWindow, WM_KEYDOWN, key, LP);
+        PostMessage( hWindow, WM_KEYUP, key, LP or $C0000000);
+      End;
+      (* process the messages *)
+      Application.ProcessMessages;
+
+      (* restore the old key state map *)
+      SetKeyboardState( pKeyBuffers^[1] );
+    finally
+      (* free the memory for the key state buffers *)
+      If pKeyBuffers <> Nil Then
+        Dispose( pKeyBuffers );
+    End; { If }
+  End;
+End; { PostKeyEx }
+
+{$ENDIF}
 
 
 procedure Register;
