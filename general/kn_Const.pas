c98020de2c07cde2c3b53536df622a9456830810
Daniel Prado Velasco <dpradov@users.noreply.github.com>
2026-02-19 22:32:28 +0100

    Preparing version 2.1.5.3

--- general/kn_Const.pas
+++ general/kn_Const.pas
@@ -33,8 +33,8 @@ procedure DefineConst;
 
 const
   Program_Name     = 'KeyNote NF';
-  Program_Version  = '2.1.5 .02';
-  Program_Version_Number  = '2.1.5.2';
+  Program_Version  = '2.1.5 .03';
+  Program_Version_Number  = '2.1.5.3';
   Program_Version_Date    = '19/02/2026';
   Program_License  = 'Free software, Open Source (Mozilla Public License 2.0)';
 
